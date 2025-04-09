23fa56473f95b1cf75fdeeb71a3ea2fdd1c7e071
jegersterk <jegersterk1@gmail.com>
2025-04-09 12:53:38 +0200

    removed components.css and associations

--- camel-coin/app/page.tsx
+++ camel-coin/app/page.tsx
@@ -1,8 +1,5 @@
-// import Image from "next/image";
 import Test from "../components/test";
 import "../styles/globals.css"
-import "../styles/components.css"
-// import { title } from "process";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
